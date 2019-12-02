2ac8b6ece47ce422ef6c66f4e6656f6b028d7030
Preston Hudson <prestonleehudson@gmail.com>
2019-12-02 11:18:57 -0600

    Added start to the game.

--- hangman.py
+++ hangman.py
@@ -1,4 +1,17 @@
 #Hangman Preston Hudson 12/1/2019 Written in Python
 
-def hangman(words):
-    return words
+def hangman(word):
+    wrong = 0
+    stages = ["",
+             "_________       ",
+             "|               ",
+             "|        |      ",
+             "|        O      ",
+             "|       /|\     ",
+             "|       / \     ",
+             "                "
+             ]
+             rletters = list(word)
+             board = ["__"] * len(word)
+             win = False
+             print("Welcome to Hangman")
